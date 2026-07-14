b376015e6e385032e64617ff1d28b4cfe55f3566
Simon Nganga <simonndungu431@gmail.com>
2026-07-14 15:21:36 +0300

    Added async support to the api endpoints

--- Pokedex.Api/Endpoints/PokemonsEndpoints.cs
+++ Pokedex.Api/Endpoints/PokemonsEndpoints.cs
@@ -1,17 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Pokedex.Api.Data;
 using Pokedex.Api.Dtos;
+using Pokedex.Api.Models;
 
 namespace Pokedex.Api.Endpoints
 {
     public static class PokemonsEndpoints
     {
         const string GETpokemonEndpointName = "GETpokemon";
-        private static readonly List<PokemonDto> pokemons = [
-            new(1, "Pikachu", "Electric", 35, 55, 40, "ksjlkljak"),
-            new(2, "Charizard", "Fire/Flying", 78, 84, 78, "kljksl"),
-            new(3, "Bulbasur", "Grass/Poison", 45, 49, 49, "ioijwl"),
-            new(4, "Squirtle", "Water", 44, 48, 65, "klkjlaj"),
-            new(5, "Gengar", "Ghost/Poison", 60, 65, 60, "lkjlja")
-        ];
 
         public static void MapPokemonsEndpoints(this WebApplication app)
         {            
@@ -19,62 +15,97 @@ namespace Pokedex.Api.Endpoints
             var group = app.MapGroup("/pokemons");
 
             //GET/pokemon
-            group.MapGet("/", () => pokemons);
+            group.MapGet("/", async (PokemonStoreContext dbContext) 
+                => await dbContext.Pokemons
+                .Include(pokemon => pokemon.Type)
+                .Select(pokemon => new PokemonSummaryDto(
+                    pokemon.Id,
+                    pokemon.Name,
+                    pokemon.Type.Name,
+                    pokemon.Hp,
+                    pokemon.Attack,
+                    pokemon.Defense,
+                    pokemon.ImageUrl
+                ))
+                .AsNoTracking()
+                .ToListAsync());
 
             //Get/pokemon/id
-            group.MapGet("/{id}", (int id)=> {
-                var game = pokemons.Find(pokemon => pokemon.Id == id);
+            group.MapGet("/{id}", async (int id, PokemonStoreContext dbContext)=> {
+                var pokemon = await dbContext.Pokemons.FindAsync(id);
 
-                return game is null ? Results.NotFound() : Results.Ok(game);
+                return pokemon is null ? Results.NotFound() : Results.Ok(
+                    new PokemonDetailsDto(
+                        pokemon.Id,
+                        pokemon.Name,
+                        pokemon.TypeId,
+                        pokemon.Hp,
+                        pokemon.Attack,
+                        pokemon.Defense,
+                        pokemon.ImageUrl
+                    )
+                );
 
                 }).WithName(GETpokemonEndpointName);
 
             //POST/pokemon
-            group.MapPost("/", (CreatePokemonDto newPokemon) =>
+            group.MapPost("/", async(CreatePokemonDto newPokemon, PokemonStoreContext dbContext) =>
             {
-                PokemonDto pokemon = new(
-                    pokemons.Count + 1,
-                    newPokemon.Name,
-                    newPokemon.Type,
-                    newPokemon.HP,
-                    newPokemon.Attack,
-                    newPokemon.Defense,
-                    newPokemon.ImageUrl
+                Pokemon pokemon = new()
+                {
+                    Name = newPokemon.Name,
+                    TypeId = newPokemon.TypeId,
+                    Hp = newPokemon.HP,
+                    Attack = newPokemon.Attack,
+                    Defense = newPokemon.Defense,
+                    ImageUrl = newPokemon.ImageUrl
+                };
+
+                dbContext.Pokemons.Add(pokemon);
+                await dbContext.SaveChangesAsync();
+
+                PokemonDetailsDto pokemonDto = new(
+                    pokemon.Id,
+                    pokemon.Name,
+                    pokemon.TypeId,
+                    pokemon.Hp,
+                    pokemon.Attack,
+                    pokemon.Defense,
+                    pokemon.ImageUrl
                 );
 
-                pokemons.Add(pokemon);
-
-                return Results.CreatedAtRoute(GETpokemonEndpointName, new {id = pokemon.Id}, pokemon);
+                return Results.CreatedAtRoute(GETpokemonEndpointName, new {id = pokemonDto.Id}, pokemonDto);
             });
 
             // PUT/pokemon/{id}
-            group.MapPut("/{id}", (int id, UpdatePokemonDto updatePokemon) =>
+            group.MapPut("/{id}", async (PokemonStoreContext dbContext , int id, UpdatePokemonDto updatePokemon) =>
             {
-                var index = pokemons.FindIndex(pokemon => pokemon.Id == id);
+                var existingPokemon = await dbContext.Pokemons.FindAsync(id);
 
-                if(index == -1)
+                if(existingPokemon is null)
                 {
                     return Results.NotFound();
                 }
 
-                pokemons[index] = new PokemonDto(
-                    id,
-                    updatePokemon.Name,
-                    updatePokemon.Type,
-                    updatePokemon.HP,
-                    updatePokemon.Attack,
-                    updatePokemon.Defense,
-                    updatePokemon.ImageUrl
-                );
+                existingPokemon.Name = updatePokemon.Name;
+                existingPokemon.TypeId = updatePokemon.TypeId;
+                existingPokemon.Hp = updatePokemon.HP;
+                existingPokemon.Attack = updatePokemon.Attack;
+                existingPokemon.Defense = updatePokemon.Defense;
+                existingPokemon.ImageUrl = updatePokemon.ImageUrl;
+
+                await dbContext.SaveChangesAsync();
 
                 return Results.NoContent();
 
             });
 
             // DELETE/pokemon/{id}
-            group.MapDelete("/{id}", (int id) => {
+            group.MapDelete("/{id}", async (int id, PokemonStoreContext dbContext) => {
                 
-                pokemons.RemoveAll(pokemon => pokemon.Id == id);
+               await dbContext.Pokemons
+                                    .Where(pokemon => pokemon.Id == id)
+                                    .ExecuteDeleteAsync();
 
                 return Results.NoContent();
             });
