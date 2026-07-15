11d3d1d13243f578510338de2d9d5df361dc11b7
Simon Nganga <simonndungu431@gmail.com>
2026-07-15 07:46:42 +0300

    changed Type to TypeId

--- Pokedex.Api/Dtos/CreatePokemonDto.cs
+++ Pokedex.Api/Dtos/CreatePokemonDto.cs
@@ -5,7 +5,7 @@ namespace Pokedex.Api.Dtos
 {
     public record CreatePokemonDto(
         [Required][StringLength(50)]string Name,
-        [Required][StringLength(20)]string Type,
+        [Range(1,50)]int TypeId,
         [Range(1, 100)]int HP, //Health Points
         [Range(1, 100)]int Attack, //Atack Strength
         [Range(1, 100)]int Defense, //Defense Strength
